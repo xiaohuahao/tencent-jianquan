a63ff9bca666169fd203d3b0f061fd5b325a8a10
张豪 <1169756883@qq.com>
2019-06-25 15:38:20 +0800

    Initial Commit

---
+++ TestDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDome
+//
+//  Created by 张豪 on 2019/6/25.
+//  Copyright © 2019 张豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDome
+//
+//  Created by 张豪 on 2019/6/25.
+//  Copyright © 2019 张豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
